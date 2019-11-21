9927a681787b6d9b9a3c757ffd3f520b53e69380
unknown <almaguero95@gmail.com>
2019-11-20 21:54:41 -0500

    added logic for comparison

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,8 +4,8 @@ var friendsArray = [
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
             5,
+            2,
             1,
-            4,
             4,
         ]
     },
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -30,10 +30,91 @@ app.post("/api/friends", function(req,res) {
 
     console.log(newFriend);
 
-    friendsArray.push(newFriend);
+    friends.push(newFriend);
 
     res.json(newFriend);
 
+    
+    
+
+    console.log("new friend array is " + newFriend.scores[0]);
+    console.log("first array score is " + friends[0].scores[0]);
+
+    var oliver = 0;
+    var oliver2 = 0;
+    var newArray = [];
+    var sum1 = 0;
+    var sum2 = 0;
+    var sum3 = 0;
+    var sum4 = 0;
+    var total = 0;
+    var total2 = 0;
+    
+    function compare(numberArray) {
+    for (var i=0;i<newFriend.scores.length;i++) {
+        oliver = newFriend.scores[i];
+        oliver2 = friends[numberArray].scores[i];
+        newArray.push(oliver);
+        newArray.push(oliver2);
+
+        if (newArray[0] > newArray[1]) {
+          sum1 = newArray[0] - newArray[1];
+        } else if (newArray[1] > newArray[0]) {
+          sum1 = newArray[1] - newArray[0];
+        }
+
+        if (newArray[2] > newArray[3]) {
+          sum2 = newArray[2] - newArray[3];
+        } else if (newArray[3] > newArray[2]) {
+          sum2 = newArray[3] - newArray[2];
+        }
+
+        if (newArray[4] > newArray[5]) {
+          sum3 = newArray[4] - newArray[5];
+        } else if (newArray[5] > newArray[4]) {
+          sum3 = newArray[5] - newArray[4];
+        }
+
+        if (newArray[6] > newArray[7]) {
+          sum4 = newArray[6] - newArray[7];
+        } else if (newArray[7] > newArray[6]) {
+          sum4 = newArray[7] - newArray[6];
+        }
+        total = sum1+sum2+sum3+sum4;
+        
+
+      }
+    }
+
+    compare(0);
+    compare(1);
+    compare(2);
+
+
+     console.log("total is " + total);
+      console.log(newArray);
+
+
+
+
+    // compare(newFriend, friends);
+
+    // function compare (arr1, arr2) {
+    //   var newArray = [];
+    //   if (arr1.scores[0] > arr2[0].scores[0]) {
+    //       arr1.scores[0] - arr2[0].scores[0];
+    //       newArray.push(arr1);
+    //   } else if (arr1.scores[0] < arr2[0].scores[0]) { 
+    //       arr2[0].scores[0] - arr10.scores[0];
+    //       newArray.push(arr2);
+    //   }
+    //   console.log(newArray);
+    // }
+
+    
+
+  
+  
 });
 
 
